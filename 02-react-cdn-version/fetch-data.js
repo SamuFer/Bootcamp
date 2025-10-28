730046e549ff5d853d822ccb675e24438726c9c3
SamuFer <fernandezSamuel0107@gmail.com>
2025-10-28 18:12:28 +0100

    clase #4 - Punt 0.2 -> React-cdn => Introduccion a react, diferencia entre usar useState vs innerHTML, creacion de componentes. Esta version utiliza un React externo con inyeccion de Babel. (version solo para aprendizaje de conceptos claves sobre React)

---
+++ 02-react-cdn-version/fetch-data.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.jobs-listings')
+
+const RESULTS_PER_PAGE = 3
+
+const botonSiguiente = document.querySelector('#siguiente')
+botonSiguiente.addEventListener('click', function (event) {
+  const element = event.target
+  console.log(element)
+})
+
+fetch("./data.json") /* fetch es asíncrono */
+  .then((response) => {
+    return response.json()
+  }) 
+  .then((jobs) => {
+    const TOTAL_ELEMENTOS = jobs.length
+    let NUM_OF_PAGES = TOTAL_ELEMENTOS / RESULTS_PER_PAGE 
+    let PAGE_NUMBER = 1
+
+    const RESULTS_TO_SKIP = (PAGE_NUMBER - 1) * RESULTS_PER_PAGE;
+    const RESULTS = jobs.slice(RESULTS_TO_SKIP, RESULTS_PER_PAGE + RESULTS_TO_SKIP);
+    console.log(RESULTS) 
+
+    jobs.forEach(job => {
+      const article = document.createElement('article')
+      article.className = 'job-listing-card'
+
+      article.dataset.modalidad = job.data.modalidad
+      article.dataset.technology = job.data.technology
+      article.dataset.nivel = job.data.nivel
+
+      article.innerHTML = `<div>
+          <h3>${job.titulo}</h3>
+          <small>${job.empresa} | ${job.ubicacion}</small>
+          <p>${job.descripcion}</p>
+        </div>
+        <button class="button-apply-job">Aplicar</button>`
+
+      container.appendChild(article)
+      
+    })
+  })
+
+
